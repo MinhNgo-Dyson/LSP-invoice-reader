280fe7e75ac7806d53df1871a93074032d943ae2
MinhNgo-Dyson <chanhanhminh.ngo@dyson.com>
2024-08-28 14:57:56 +0800

    version 20240828

--- invoice_extract.py
+++ invoice_extract.py
@@ -1,22 +1,24 @@
 import re
 
+
 def get_lsp(lines):
     lsp_set = set()
     for line in lines:
-        if 'ceva' in line.lower():
+        if 'ceva freight' in line.lower():
             lsp_set.add('ceva')
-        if 'dhl' in line.lower():
+        if 'dhl global' in line.lower():
             lsp_set.add('dhl')
-        if 'maersk' in line.lower():
+        if 'maersk malaysia' in line.lower():
             lsp_set.add('maersk')
 
     lsp_list = list(lsp_set)
     if len(lsp_list) == 0:
-        return None
+        return "Unknown"
     if len(lsp_list) > 1:
         return "Unknown: " + str(lsp_list)
 
     return lsp_list[0]
+
 
 def extract_invoice_number_ceva(lines):
     for i, line in enumerate(lines):
@@ -30,25 +32,27 @@ def extract_invoice_number_ceva(lines):
                     return invoice_number
     return None
 
+
 def extract_total_amount_ceva(lines):
     total_amount_usd = None
     total_amount_myr = None
     ex_rate = None
-    
+
     for i, line in enumerate(lines):
         match_myr = re.search(r'(\d+\.\d{2})MYR', line.replace(" ", ""), re.I)
         match_usd = re.search(r'(\d+\.\d{2})USD', line.replace(" ", ""), re.I)
-        match_rate_temp = re.search(r'(?<!Tax)EXCHANGERATE', line.replace(" ", ""), re.I)
+        match_rate_temp = re.search(
+            r'(?<!Tax)EXCHANGERATE', line.replace(" ", ""), re.I)
         if match_myr:
             total_amount_myr = float(match_myr.group(1))
         if match_usd:
             total_amount_usd = float(match_usd.group(1))
-            
-        
+
         if match_rate_temp and not ex_rate:
             if i + 1 < len(lines):
                 next_line = lines[i+1]
-                match_usd_rate = re.search(r'(\d+\.\d{2})USDx(\d+\.\d{6})', next_line.replace(" ", ""), re.I)
+                match_usd_rate = re.search(
+                    r'(\d+\.\d{2})USDx(\d+\.\d{6})', next_line.replace(" ", ""), re.I)
                 if match_usd_rate:
                     total_amount_usd = float(match_usd_rate.group(1))
                     ex_rate = float(match_usd_rate.group(2))
@@ -60,9 +64,10 @@ def extract_invoice_number_dhl(lines):
     for line in lines:
         match = re.search(r'INVOICE (W\d+)', line, re.I)
         if match:
-            invoice_number =  match.group(1)
+            invoice_number = match.group(1)
 
     return invoice_number
+
 
 def extract_total_amount_dhl(lines):
     total_amount_usd = None
@@ -70,9 +75,12 @@ def extract_total_amount_dhl(lines):
     ex_rate = None
 
     for line in lines:
-        match_usd = re.search(r'TOTALUSD\D*(\d+\.\d{2})', line.replace(" ", ""), re.I)
-        match_myr = re.search(r'(?:MYRSUBTOTAL|TOTALMYR)\D*(\d+\.\d{2})', line.replace(" ", ""), re.I)
-        match_rate = re.search(r'USD\d+\.\d{2}@(\d+\.\d{6})', line.replace(" ", ""), re.I)
+        match_usd = re.search(
+            r'TOTALUSD\D*(\d+\.\d{2})', line.replace(" ", ""), re.I)
+        match_myr = re.search(
+            r'(?:MYRSUBTOTAL|TOTALMYR)\D*(\d+\.\d{2})', line.replace(" ", ""), re.I)
+        match_rate = re.search(
+            r'USD\d+\.\d{2}@(\d+\.\d{6})', line.replace(" ", ""), re.I)
 
         if match_usd and not total_amount_usd:
             total_amount_usd = float(match_usd.group(1))
@@ -87,8 +95,9 @@ def extract_invoice_number_msk(lines):
     for line in lines:
         match = re.search(r'InvoiceNumber(\d+)', line.replace(" ", ""), re.I)
         if match:
-            invoice_number =  match.group(1)
+            invoice_number = match.group(1)
     return invoice_number
+
 
 def extract_total_amount_msk(lines):
     total_amount_usd = None
@@ -96,15 +105,19 @@ def extract_total_amount_msk(lines):
     ex_rate = None
 
     for line in lines:
-        match_usd = re.search(r'TotalBaseAmount\W*USD\D*(\d+\.\d{2})', line.replace(" ", ""), re.I)
-        match_myr = re.search(r'TotalBaseAmount\W*MYR\D*(\d+\.\d{2})', line.replace(" ", ""), re.I)
-        match_rate = re.search(r'ExchangeRateConversionUSDtoMYR\D*(\d+\.\d+)', line.replace(" ", ""), re.I)
+        match_usd = re.search(
+            r'TotalBaseAmount\W*USD\D*(\d+\.\d{2})', line.replace(" ", ""), re.I)
+        match_myr = re.search(
+            r'TotalBaseAmount\W*MYR\D*(\d+\.\d{2})', line.replace(" ", ""), re.I)
+        match_rate = re.search(
+            r'ExchangeRateConversionUSDtoMYR\D*(\d+\.\d+)', line.replace(" ", ""), re.I)
         if match_usd and not total_amount_usd:
             total_amount_usd = float(match_usd.group(1))
         if match_myr and not total_amount_myr:
             total_amount_myr = float(match_myr.group(1))
         if match_rate and not ex_rate:
             ex_rate = match_rate.group(1)
-            ex_rate = float(round(1/float(ex_rate),4) if float(ex_rate) < 1 else round(float(ex_rate),4))
+            ex_rate = float(round(1/float(ex_rate), 4)
+                            if float(ex_rate) < 1 else round(float(ex_rate), 4))
 
     return total_amount_usd, total_amount_myr, ex_rate
--- invoice_reader.py
+++ invoice_reader.py
@@ -8,15 +8,18 @@ import pytesseract
 
 from invoice_extract import extract_invoice_number_ceva, extract_total_amount_ceva, extract_invoice_number_dhl, extract_total_amount_dhl, extract_invoice_number_msk, extract_total_amount_msk, get_lsp
 
-POPPLER_PATH = "../Support/poppler-24.02.0/Library/bin"
-TESSERACT_PATH = "../Support/Tesseract-OCR/tesseract.exe"
+POPPLER_PATH = "Support/poppler-24.02.0/Library/bin"
+TESSERACT_PATH = "Support/Tesseract-OCR/tesseract.exe"
+
 
 def pdf_to_images(pdf_path, max_pages=3, poppler_path=None):
     if poppler_path:
-        images = convert_from_path(pdf_path, first_page=1, last_page=max_pages, poppler_path=poppler_path)
+        images = convert_from_path(
+            pdf_path, first_page=1, last_page=max_pages, poppler_path=poppler_path)
     else:
         images = convert_from_path(pdf_path, first_page=1, last_page=max_pages)
     return images
+
 
 def ocr_image(image, tesseract_path=None):
     if tesseract_path:
@@ -24,11 +27,13 @@ def ocr_image(image, tesseract_path=None):
     text = pytesseract.image_to_string(image)
     return text
 
+
 def preprocess_text(text):
     lines = text.split('\n')
     lines = [line.strip() for line in lines if line.strip() != '']
-    lines = [line.replace(",","") for line in lines]
+    lines = [line.replace(",", "") for line in lines]
     return lines
+
 
 def process_invoice(pdf_path, poppler_path=None, tesseract_path=None):
     images = pdf_to_images(pdf_path, 3, poppler_path)
@@ -38,14 +43,25 @@ def process_invoice(pdf_path, poppler_path=None, tesseract_path=None):
 
     if lsp == "ceva":
         invoice_number = extract_invoice_number_ceva(lines)
-        total_amount_usd, total_amount_myr, exchange_rate = extract_total_amount_ceva(lines)
+        total_amount_usd, total_amount_myr, exchange_rate = extract_total_amount_ceva(
+            lines)
     elif lsp == "dhl":
         invoice_number = extract_invoice_number_dhl(lines)
-        total_amount_usd, total_amount_myr, exchange_rate = extract_total_amount_dhl(lines)
+        total_amount_usd, total_amount_myr, exchange_rate = extract_total_amount_dhl(
+            lines)
     elif lsp == "maersk":
         invoice_number = extract_invoice_number_msk(lines)
-        total_amount_usd, total_amount_myr, exchange_rate = extract_total_amount_msk(lines)
-    
+        total_amount_usd, total_amount_myr, exchange_rate = extract_total_amount_msk(
+            lines)
+    else:
+        return {
+            'lsp': "Unknown",
+            'invoice_number': "Unknown",
+            'total_amount_usd': "Unknown",
+            'total_amount_myr': "Unknown",
+            'exchange_rate': 1
+        }
+
     return {
         'lsp': lsp,
         'invoice_number': invoice_number,
@@ -54,16 +70,19 @@ def process_invoice(pdf_path, poppler_path=None, tesseract_path=None):
         'exchange_rate': exchange_rate
     }
 
+
 def process_invoices_in_folder(folder_path, poppler_path=None, tesseract_path=None):
     results = []
     for root, _, files in os.walk(folder_path):
         for filename in files:
             if filename.lower().endswith('.pdf'):
                 pdf_path = os.path.join(root, filename)
-                result = process_invoice(pdf_path, poppler_path, tesseract_path)
+                result = process_invoice(
+                    pdf_path, poppler_path, tesseract_path)
                 result['file'] = pdf_path
                 results.append(result)
     return results
+
 
 def export_to_excel(results, output_folder_path):
     df = pd.DataFrame(results)
@@ -75,24 +94,27 @@ def export_to_excel(results, output_folder_path):
     df.to_excel(output_excel, index=False)
     return None
 
+
 if __name__ == "__main__":
     try:
-        folder_path = input("What is the invoice folder?").replace("\"", "").replace("\'", "")
+        folder_path = input("What is the invoice folder?").replace(
+            "\"", "").replace("\'", "")
         # Start the timer
         start_time = time.time()
         print("Processing invoices ...")
-        results = process_invoices_in_folder(folder_path, POPPLER_PATH, TESSERACT_PATH)
+        results = process_invoices_in_folder(
+            folder_path, POPPLER_PATH, TESSERACT_PATH)
         # End the timer
         end_time = time.time()
 
         elapsed_time = end_time - start_time
         print(f"The code took {elapsed_time:.4f} seconds to run.")
-        
-        output_folder_path = input("What is the export folder?").replace("\"", "").replace("\'", "")
+
+        output_folder_path = input("What is the export folder?").replace(
+            "\"", "").replace("\'", "")
         export_to_excel(results, output_folder_path)
         print("Exporting invoices ...")
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
         input("Press Enter to exit...")
-        
